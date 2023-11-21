8796a9eae08a83b7d10e701a1d8639613089bb27
Bones <heaugo@gmail.com>
2023-11-20 19:25:57 -0500

    Refactor XMLDataService struct and add new fields for storing vote results endpoint to parse file xml into readble object db work can starts

--- models/xmlDataService.go
+++ models/xmlDataService.go
@@ -19,26 +19,39 @@ type XMLDataService struct {
 
 type XMLRollCallVoteResults struct {
 	XMLName xml.Name    `xml:"PV.RollCallVoteResults"`
-	Titles  []string    `xml:"RollCallVoteResults.Titles"`
+	Titles  []Title     `xml:"RollCallVoteResults.Titles>RollCallVoteResults.Title.Text"`
 	Result  []XMLResult `xml:"RollCallVote.Result"`
 }
 
-type XMLResult struct {
-	XMLName     xml.Name        `xml:"RollCallVote"`
-	Description string          `xml:"RollCallVote.Description.Text"`
-	For         []XMLVoteResult `xml:"Result.For"`
-	Against     []XMLVoteResult `xml:"Result.Against"`
-	Abstention  []XMLVoteResult `xml:"Result.Abstention"`
-	Intentions  []XMLVoteResult `xml:"Intentions"`
+type Title struct {
+	Text     string `xml:",chardata"`
+	Language string `xml:"Language,attr"`
 }
 
-type XMLVoteResult struct {
-	Member []XMLMember `xml:"Member"`
+type XMLResult struct {
+	XMLName     xml.Name `xml:"RollCallVote.Result"`
+	Description string   `xml:"Description.Text"`
+	For         XMLVote  `xml:"Result.For"`
+	Against     XMLVote  `xml:"Result.Against"`
+	Abstention  XMLVote  `xml:"Result.Abstention"`
+	Intentions  string   `xml:"Intentions Number,attr"`
+}
+
+type XMLVote struct {
+	Count          int                 `xml:"Number,attr"`
+	PoliticalGroup []XMLPoliticalGroup `xml:"Result.PoliticalGroup.List"`
+}
+
+type XMLPoliticalGroup struct {
+	// XMLName   xml.Name    	`xml:"PoliticalGroup.List"`
+	Identifier string      `xml:"Identifier,attr"`
+	Members    []XMLMember `xml:"PoliticalGroup.Member.Name"`
 }
 
 type XMLMember struct {
-	Name           string `xml:"PoliticalGroup.Member.Name"`
-	PoliticalGroup string `xml:"Result.PoliticalGroup.List Identifier"`
+	Name   string `xml:",chardata"`
+	MepId  int    `xml:"MepId,attr"`
+	PersId int    `xml:"PersId,attr"`
 }
 
 func (s *XMLDataService) IngestData(rawXMLData []byte) error {
@@ -58,15 +71,48 @@ func (s *XMLDataService) IngestData(rawXMLData []byte) error {
 	if len(data.Result) > 0 {
 		for i := 0; i < len(data.Result); i++ {
 			log.Println("RollCallVote:", i)
+			for j := 0; j < len(data.Titles); j++ {
+				log.Println("Title:", data.Titles[j].Text)
+				log.Println("Language:", data.Titles[j].Language)
+			}
 			log.Println("Description:", data.Result[i].Description)
-			log.Println("For:", len(data.Result[i].For))
-			log.Println("Against:", len(data.Result[i].Against))
-			log.Println("Abstention:", len(data.Result[i].Abstention))
+			log.Println("For:", data.Result[i].For)
+			log.Println("Count:", data.Result[i].For.Count)
+			log.Println("PoliticalGroup:", data.Result[i].For.PoliticalGroup)
+			for k := 0; k < len(data.Result[i].For.PoliticalGroup); k++ {
+				log.Println("PoliticalGroup:", data.Result[i].For.PoliticalGroup[k].Identifier)
+				for l := 0; l < len(data.Result[i].For.PoliticalGroup[k].Members); l++ {
+					log.Println("Member:", data.Result[i].For.PoliticalGroup[k].Members[l].Name)
+					log.Println("MepId:", data.Result[i].For.PoliticalGroup[k].Members[l].MepId)
+					log.Println("PersId:", data.Result[i].For.PoliticalGroup[k].Members[l].PersId)
+				}
+			}
+			log.Println("Against:", data.Result[i].Against)
+			log.Println("Count:", data.Result[i].Against.Count)
+			for m := 0; m < len(data.Result[i].Against.PoliticalGroup); m++ {
+				log.Println("PoliticalGroup:", data.Result[i].Against.PoliticalGroup[m].Identifier)
+				for n := 0; n < len(data.Result[i].Against.PoliticalGroup[m].Members); n++ {
+					log.Println("Member:", data.Result[i].Against.PoliticalGroup[m].Members[n].Name)
+					log.Println("MepId:", data.Result[i].Against.PoliticalGroup[m].Members[n].MepId)
+					log.Println("PersId:", data.Result[i].Against.PoliticalGroup[m].Members[n].PersId)
+				}
+			}
 
+			log.Println("Abstention:", data.Result[i].Abstention)
+			log.Println("Count:", data.Result[i].Abstention.Count)
+			for k := 0; k < len(data.Result[i].Abstention.PoliticalGroup); k++ {
+				log.Println("PoliticalGroup:", data.Result[i].Abstention.PoliticalGroup[k].Identifier)
+				for l := 0; l < len(data.Result[i].Abstention.PoliticalGroup[k].Members); l++ {
+					log.Println("Member:", data.Result[i].Abstention.PoliticalGroup[k].Members[l].Name)
+					log.Println("MepId:", data.Result[i].Abstention.PoliticalGroup[k].Members[l].MepId)
+					log.Println("PersId:", data.Result[i].Abstention.PoliticalGroup[k].Members[l].PersId)
+				}
+			}
 		}
 	} else {
-		fmt.Printf("No data found\n")
+		log.Println("No data")
 	}
+
 	// Access the parsed data
 	// fmt.Println("Title Text:", data.Titles[0].Text)
 	// fmt.Println("Field2:", data.XMLName)
